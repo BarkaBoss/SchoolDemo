79edf7f13a10657cca223b8e653dcab87c0fdede
BarkaBoss <bossmajor.biii@gmail.com>
2024-03-02 22:19:00 +0100

    Added Update Test

--- src/test/java/ng/com/nokt/school/repository/CoursesRepositoryTest.java
+++ src/test/java/ng/com/nokt/school/repository/CoursesRepositoryTest.java
@@ -1,11 +1,12 @@
 package ng.com.nokt.school.repository;
 
-import jakarta.validation.constraints.NotNull;
 import ng.com.nokt.school.domain.Courses;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -24,6 +25,8 @@ class CoursesRepositoryTest {
         assertThat(courses.size()).isEqualTo(5);
     }
 
+    @Transactional
+    @Rollback
     @Test
     void saveNewCourse(){
         Courses course = Courses.builder()
@@ -36,5 +39,20 @@ class CoursesRepositoryTest {
 
         assertThat(newCourse).isNotNull();
         assertThat(newCourse.getId()).isNotNull();
+    }
+
+    @Test
+    void testUpdateCourse(){
+        Courses courses = coursesRepository.findAll().get(0);
+        Courses updateCourse = Courses.builder()
+                .id(courses.getId())
+                .courseTitle("Java Java Bills")
+                .courseCode("CMP725").build();
+
+        Courses newCourse = coursesRepository.save(updateCourse);
+        coursesRepository.flush();
+        assertThat(newCourse).isNotNull();
+        assertThat(newCourse.getId()).isEqualTo(courses.getId());
+        assertThat(newCourse.getCourseTitle()).isEqualTo(updateCourse.getCourseTitle());
     }
 }
